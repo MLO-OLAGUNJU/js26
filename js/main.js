d20f3d59d304988b148a2e60393f08375c720382
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2023-12-23 11:20:07 +0100

    javascript promises

--- js/main.js
+++ js/main.js
@@ -1,2 +1,27 @@
 //Fetch API requires a discussion of .........
 // Callbacks, Promises, Thenables, and Async/Await
+
+function firstFunction(parameters, callback) {
+  // do stuff
+  callback();
+}
+
+// AKA "CALLBACK HELL"
+firstFunction(para, function () {
+  //do stuff
+  secondFunction(para, function () {
+    thirdFunction(para, function () {});
+  });
+});
+
+// Promises
+//3 states: Pending, Fulfilled, Rejected
+
+const myPromise = new Promise((resolve, reject) => {
+  const error = false;
+  if (!error) {
+    resolve("Yes! resolved the promise!");
+  } else {
+    reject("No! rejected the the promise.");
+  }
+});
